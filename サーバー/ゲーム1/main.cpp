5eeac2545245ee20a15fb48d8dc12c5751af2256
nao2006 <40855877+nao2006@users.noreply.github.com>
2018-07-09 13:01:20 +0900

    saba
    kinmedai

--- サーバー/ゲーム1/main.cpp
+++ サーバー/ゲーム1/main.cpp
@@ -3,6 +3,8 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
+#define printf
+
 int main()
 {
 	WSADATA wsaData;
@@ -11,21 +13,42 @@ int main()
 	struct sockaddr_in client;
 	int len;
 	SOCKET sock;
+	int n;
 
 	// winsock2の初期化
-	WSAStartup(MAKEWORD(2, 0), &wsaData);
+	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+	{
+		return 1;
+	}
 
 	// ソケットの作成
 	sock0 = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock0 == INVALID_SOCKET) 
+	{
+		printf("socket : %d\n", WSAGetLastError());
+		return 1;
+	}
 
 	// ソケットの設定
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(12345);
 	addr.sin_addr.S_un.S_addr = INADDR_ANY;
-	bind(sock0, (struct sockaddr *)&addr, sizeof(addr));
+	//bind(sock0, (struct sockaddr *)&addr, sizeof(addr));
+
+	if (bind(sock0, (struct sockaddr *)&addr, sizeof(addr)) != 0) 
+	{
+		printf("bind : %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	if (listen(sock0, 5) != 0) 
+	{
+		printf("listen : %d\n", WSAGetLastError());
+		return 1;
+	}
 
 	// TCPクライアントからの接続要求を待てる状態にする
-	listen(sock0, 5);
+	//listen(sock0, 5);
 
 	while (1) 
 	{
@@ -33,8 +56,20 @@ int main()
 		len = sizeof(client);
 		sock = accept(sock0, (struct sockaddr *)&client, &len);
 
+		if (sock == INVALID_SOCKET) 
+		{
+			printf("accept : %d\n", WSAGetLastError());
+			break;
+		}
+
 		// 5文字送信
-		send(sock, "HELLO", 5, 0);
+		n = send(sock, "HELLO", 5, 0);
+
+		if (n < 1) 
+		{
+			printf("send : %d\n", WSAGetLastError());
+			break;
+		}
 
 		// TCPセッションの終了
 		closesocket(sock);
