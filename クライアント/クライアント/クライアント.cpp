3d7c07a1ff9e40b8cfa2af7d6ab75f770e4ae2a2
nao2006 <40855877+nao2006@users.noreply.github.com>
2018-07-26 19:09:07 +0900

    saba kuraianto
    書き直し2

--- クライアント/クライアント/クライアント.cpp
+++ クライアント/クライアント/クライアント.cpp
@@ -11,13 +11,11 @@ int main()
 	struct sockaddr_in server;
 	SOCKET sock = INVALID_SOCKET;
 	char buf[256];
-	int n = 0;
 	char *deststr = nullptr;
-	unsigned int **addrptr = nullptr;
 	u_long val = 1;
-
 	ioctlsocket(sock, FIONBIO, &val);
-	
+
+
 	// winsock2の初期化
 	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
 	{
@@ -37,77 +35,74 @@ int main()
 	// 接続先指定用構造体の準備
 	server.sin_family = AF_INET;
 	server.sin_port = htons(12345);
+
 	server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
 
-	if (server.sin_addr.S_un.S_addr != 0xffffffff)
+	if (server.sin_addr.S_un.S_addr == 0xffffffff)
 	{
 		struct hostent *host;
 
 		host = gethostbyname(deststr);
-
 		if (host == NULL)
 		{
-			if(WSAGetLastError() == WSAHOST_NOT_FOUND)
-			{
-				printf("host not found : %s\n", deststr);
-			}
+			WSAGetLastError() == WSAHOST_NOT_FOUND;
+
+			printf("host not found : %s\n", deststr);
+
 			return 1;
 		}
 
-		server.sin_addr.S_un.S_addr = *(unsigned int *)host->h_addr_list[0];
 
-		//ad0drptr = (unsigned int **)host->h_addr_list;
+		server.sin_addr.S_un.S_addr =
+			*(unsigned int *)host->h_addr_list[0];
 
-		while (addrptr != nullptr)
-		{
-			server.sin_addr.S_un.S_addr = *(*addrptr);
-
-			//connect()が成功したらループを抜け
-			if (connect(sock, (struct sockaddr *)&server, sizeof(server)) == 0);
-			{
-				//break;
-			}
-
-			addrptr++;
-			//connectが失敗したらつぎのアドレスでお試し
-		}
-
-		//connectがすべて失敗した場合下のif文に入る
-		if (addrptr == nullptr)
-		{
-			printf("connect	: %d\n", WSAGetLastError());
-			return 1;
-		}
 	}
-	else
+
+
+	InetPton(server.sin_family, L"127.0.0.1", &server.sin_addr.S_un.S_addr);
+
+	// サーバに接続
+	connect(sock, (struct sockaddr *)&server, sizeof(server));
+	while (1)
 	{
-		//inet_addr()が成功したとき
-		// サーバに接続
-		if (connect(sock, (struct sockaddr *)&server, sizeof(server)) != 0)
+		// サーバからデータを受信
+		memset(buf, 0, sizeof(buf));
+		int n = recv(sock, buf, sizeof(buf), 0);
+		if (n > 0)
 		{
-			printf("connect : %d\n", WSAGetLastError());
-			return 1;
+			printf("%s", buf);
 		}
-	}
-	
-	//本当はconnect()が成功したかどうかを判断してから次に進む
 
-	// サーバからデータを受信
-	memset(buf, 0, sizeof(buf));
-	n = recv(sock, buf, sizeof(buf), 0);
-
-	if (n < 0)
-	{
-		printf("recv : %d\n", WSAGetLastError());
-		return 1;
+		memset(buf, 0, sizeof(buf));
+		scanf_s("%s", buf);
 
 		// 文字送信
 		n = send(sock, buf, 256, 0);
+
+		if (n < 1)
+		{
+			if (WSAGetLastError() == WSAEWOULDBLOCK)
+			{
+				//まだできないよ
+				printf("だめ\n");
+			}
+			else
+			{
+				printf("recevied data\n");
+				printf("%s\n", buf);
+			}
+
+			//とりあえず一一秒待ち
+			Sleep(1000);
+			printf("send : %d\n", WSAGetLastError());
+
+		}
+
+		if (n > 0)
+		{
+			printf("%d, %s\n", n, buf);
+		}
 	}
-
-	printf("%d, %s\n", n, buf);
-
-	
 	closesocket(sock);
 
 	// winsock2の終了処理
--- サーバー/ゲーム1/サーバー.cpp
+++ サーバー/ゲーム1/サーバー.cpp
@@ -1,11 +1,9 @@
-#define  _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <Winsock2.h>
 #include <WS2tcpip.h>
 #include <iostream>
 
 #pragma comment(lib, "ws2_32.lib")
-
-#define PORT 50001
 
 
 int main()
@@ -18,78 +16,108 @@ int main()
 	SOCKET sock = INVALID_SOCKET;
 	int n;
 	BOOL yes = 1;
-	char buf[256];
 
 	// winsock2の初期化
 	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
 	{
+		printf("ソケット初期化：失敗\n");
 		return 1;
 	}
 
-	// ソケット作成
+	// ソケットの作成
 	sock0 = socket(AF_INET, SOCK_STREAM, 0);
-
 	if (sock0 == INVALID_SOCKET)
 	{
 		printf("socket : %d\n", WSAGetLastError());
 		return 1;
 	}
 
-	// ソケット設定
+	// ソケットの設定
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(PORT);
+	addr.sin_port = htons(12345);
 	addr.sin_addr.S_un.S_addr = INADDR_ANY;
 
-	//関連付け
-	if (bind(sock0, (struct sockaddr *)&addr, sizeof(addr))!= 0)
+	bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+
+	//ここでノンブロッキングに設定
+	//val = 0でブロッキングモードに設定
+	//ソケットの初期設定はブロッキングモードです
+	u_long val = 1;
+
+	ioctlsocket(sock, FIONBIO, &val);
+
+	setsockopt(sock0, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes));
+
+	if (bind(sock0, (struct sockaddr *)&addr, sizeof(addr)) != 0)
 	{
 		printf("bind : %d\n", WSAGetLastError());
 		return 1;
 	}
-	
-	//TCPクライアントからの接続要求を持てる状態にします
+
 	if (listen(sock0, 5) != 0)
 	{
 		printf("listen : %d\n", WSAGetLastError());
 		return 1;
 	}
 
-	// TCPクライアントからの接続要求を受け付ける
-	len = sizeof(client);
-	while (sock == INVALID_SOCKET)
+	// TCPクライアントからの接続要求を待てる状態にする
+	//listen(sock0, 5);
+
+	while (1)
 	{
+		// TCPクライアントからの接続要求を受け付ける
+		len = sizeof(client);
 		sock = accept(sock0, (struct sockaddr *)&client, &len);
-		if (sock != INVALID_SOCKET)
-		{
-			break;
-		}
-	}
 
-	//TCPクライアントのセッション事の処理
-	while (true)
-	{
-		// 文字送信
-		n = send(sock, buf, 256, 0);
-
-		if (n < 1)
-		{
-			printf(" send : %d\n", WSAGetLastError());
-		}
 
 		printf("accepted connection from %s, port=%d\n",
 			inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 
-		
-		//TCPセッションの終了
+
 		if (sock == INVALID_SOCKET)
 		{
-			closesocket(sock);
+			printf("accept : %d\n", WSAGetLastError());
 			break;
 		}
-	}
 
-	//winsock2の終了処理
+		char buf[256];
+
+		while (true)
+		{
+			memset(buf, 0, sizeof(buf));
+
+			scanf_s("%s", buf);
+
+			// 文字送信
+			n = send(sock, buf, 256, 0);
+
+		}
+
+		if (n < 1)
+		{
+			if (WSAGetLastError() == WSAEWOULDBLOCK)
+			{
+				//まだできないよ
+				printf("だめ\n");
+			}
+
+			else
+			{
+				printf("recevied data\n");
+				printf("%s\n", buf);
+				break;
+			}
+
+			Sleep(1000);
+			printf("send : %d\n", WSAGetLastError());
+			break;
+		}
+
+		// TCPセッションの終了
+		closesocket(sock);
+	}
+	// winsock2の終了処理
 	WSACleanup();
 
-	return 0l;
+	return 0;
 }
